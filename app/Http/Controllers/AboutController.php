006df04596f10f122c7679fe959bf6ea18e4fd9c
@Thebackend <kumferdinand620@gmail.com>
2023-11-21 10:13:37 +0100

    first

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function indexA(){
+        return view('about');
+
+    }
+}
